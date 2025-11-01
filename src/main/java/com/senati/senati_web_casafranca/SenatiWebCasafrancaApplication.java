fcacce30e5800007645e2eb9e43c71a6802b2235
RamsesCL <1594608@senati.pe>
2025-11-01 18:54:31 -0500

    correccion

--- src/main/java/com/senati/senati_web_casafranca/SenatiWebCasafrancaApplication.java
+++ src/main/java/com/senati/senati_web_casafranca/SenatiWebCasafrancaApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SenatiWebCasafrancaApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SenatiWebCasafrancaApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SenatiWebCasafrancaApplication.class, args);
+    }
 }
